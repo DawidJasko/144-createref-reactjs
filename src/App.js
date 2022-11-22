82504ed9cbdf34c5682299f1977023a692f84491
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2022-11-22 16:48:14 +0100

    add input adn button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React, { PureComponent } from 'react';
+import React, { createRef, PureComponent } from 'react';
 import './style.css';
 
 class App extends PureComponent {
+  textInput = createRef();
+  paragraphElement = createRef();
+
   render() {
-    return <div>{'Samuraj Programowanie kurs React od podstaw'}</div>;
+    console.log(this.textInput);
+    return (
+      <div>
+        <input ref={this.textInput} type="text" />
+        <input type="number" />
+        <p ref={this.paragraphElement}>Hello</p>
+        <button onClick={this.focusTextInput}>Ustaw focus na input</button>
+        <button 
+        onClick={this.addChar}
+        >Dodaj !</button>
+      </div>
+    );
   }
+
+  focusTextInput = () => this.textInput.current.focus();
+  addChar = () => this.paragraphElement.current.textContent += '!' ;
 }
 
 export default App;
